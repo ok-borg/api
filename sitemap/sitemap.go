56848fde0ddc1b3bf2530ad39f5e572c0813095f
crufter <dobronszki@gmail.com>
2017-02-02 16:59:00 +0000

    Prerender

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -7,17 +7,41 @@ import (
 	"github.com/joeguo/sitemap"
 	"github.com/ok-borg/borg/types"
 	"gopkg.in/olivere/elastic.v3"
+	"html/template"
+	"os"
 	"reflect"
 	"time"
 )
 
+const (
+	base = "https://ok-b.org/t/"
+)
+
+var prerenderTemplate = `
+<html>
+<head>
+</head>
+	<title>{{.problem.Title}}</title>
+<body>
+{{ range $key, $value := .problem.Solutions}}
+	{{ range $key1, $value1 := .Body }}
+	<div>{{$value1}}</div>
+	{{ end }}
+{{ end }}
+<div>
+	This html page was prerendered for google. The full page is here: <a href="{{.url}}">{{.url}}</a>
+</div>
+</body>
+</html>
+`
+
 // GenerateSitemap grabs all entries (now only ones added with borg) and saves a sitemap.xml.gz file in `sitemapPath`
 func GenerateSitemap(sitemapPath string, client *elastic.Client) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Warnf("Sitemap generation failed: %v", r)
-		}
-	}()
+	//defer func() {
+	//	if r := recover(); r != nil {
+	//		log.Warnf("Sitemap generation failed: %v", r)
+	//	}
+	//}()
 	// this query is because we only want to show user submitted content for now - not ones scraped from somewhere else - to not piss of google
 	// @TODO include ones which were changed substantially
 	// @TODO this is going to get dog slow
@@ -34,8 +58,26 @@ func GenerateSitemap(sitemapPath string, client *elastic.Client) {
 	}
 	items := []*sitemap.Item{}
 	for _, v := range all {
+		err := os.MkdirAll("/tmp/"+v.Id, 0777)
+		if err != nil {
+			panic(err)
+		}
+		slug := fmt.Sprintf("%v/%v", v.Id, slugify.S(v.Title))
+		f, err := os.Create(sitemapPath + "/" + slug + ".html")
+		if err != nil {
+			panic(err)
+		}
+		t := template.Must(template.New("single").Parse(prerenderTemplate))
+		config := map[string]interface{}{
+			"url":     base + slug,
+			"problem": v,
+		}
+		err = t.Execute(f, config)
+		if err != nil {
+			panic(err)
+		}
 		item := &sitemap.Item{
-			Loc:        "https://ok-b.org/t/" + fmt.Sprintf("%v/%v", v.Id, slugify.S(v.Title)),
+			Loc:        base + slug + ".html",
 			LastMod:    time.Now(),
 			Priority:   0.5,
 			Changefreq: "daily",
